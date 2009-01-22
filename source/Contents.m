b0f94def52a0705df9e71f3883ae66474db76741
Per.Andreas.Brodtkorb <Per.Andreas.Brodtkorb@13e78ada-cb4f-11dd-87b6-9d1a99559f42>
2009-01-22 16:55:41 +0000

    Final step in relocating all files in wafo from trunc to trunc/wafo25

--- source/Contents.m
+++
@@ -1,5 +0,0 @@
-% SOURCE
-%
-% Files
-%   compile_wafo_exe - Script that compiles WAFO exe-files.
-%   compile_wafo_mex - Script that compiles WAFO mexfiles.
--- trgauss/private/holmquist2.m
+++
@@ -1,7 +0,0 @@
-function E=holmquist2(S,Q1,Q2)
-% HOLMQUIST2 Computes moments for higher order reliability methods.
-
-S=.5*(S+S');
-Q1=.5*(Q1+Q1');
-Q2=.5*(Q2+Q2');
-E=2*trace(Q1*S*Q2*S)+trace(Q1*S)*trace(Q2*S);

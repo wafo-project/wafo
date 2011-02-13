dbb0956deadeebf3b54fb8a600af259ea728255e
georg.lindgren <georg.lindgren@gmail.com>
2011-02-13 19:29:19 +0000

    Added module and updated Contents

--- wafo25/numdifftools/Contents.m
+++ wafo25/numdifftools/Contents.m
@@ -1,5 +1,5 @@
 % Module NUMDIFFTOOLS in WAFO Toolbox. 
-%  Version 2.1.1   15-sep-2007 
+%  Version 2.5.2   07-Feb-2011 
 %  
 %  Readme           - Readme file for module NUMDIFFTOOLS in WAFO Toolbox.
 % 

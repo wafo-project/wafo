6cc3eda60997d93320ad7225e1b0afd2b25c38ba
georg.lindgren@gmail.com <georg.lindgren@gmail.com@13e78ada-cb4f-11dd-87b6-9d1a99559f42>
2011-02-13 19:34:28 +0000

    Updated Content to Ver 2.5 and revised help text

--- trunk/wafo25/Contents.m
+++ trunk/wafo25/Contents.m
@@ -1,5 +1,5 @@
 % WAFO Toolbox
-% Version 2.2.1   07-Aug-2008 
+% Version 2.5.2   07-Feb-2011 
 % 
 %   Wave Analysis for Fatigue and Oceanography (WAFO) Toolbox
 % 
@@ -16,42 +16,47 @@
 %
 % The paths to the modules are initiated by the function 'initwafo'.
 %
-% ONEDIM     - Data analysis of time series. Example: extraction of 
-%              turning points, estimation of spectrum, covariance function.
-% SPEC       - Computation of spectral moments and covariance functions. 
-%              Ex: common spectra implemented, directional spectra, 
-%              bandwidth measures
-% TRGAUSS    - Modelling with linear, Gaussian waves. Ex: exact 
-%              distributions for wave characteristics, transformed
-%              Gaussian processes
-% WAVEMODELS - Models for distributions of wave characteristics found in 
-%              the literature. Ex: parametric models for breaking 
-%              limited wave heights.
-% MULTIDIM   - Multi-dimensional time series analysis.  (Under construction)
-% CYCLES     - Cycle counting, discretization, and crossings. Ex: Rainflow 
-%              cycles and matrix, discretization of loads.
-% MARKOV     - Routines for Markov loads, switching Markov loads, and 
-%              their connection to rainflow cycles.
-% DAMAGE     - Calculation of damage. Ex: Damage of a rainflow count or 
-%              matrix, damage matrix, S-N plot.
-% SIMTOOLS   - Simulation of random processes. Ex: spectral simulation, 
-%              simulation of discrete Markov chains, switching Markov
-%              chains, harmonic oscillator
-% STATISTICS - Statistical tools and extreme-value distributions.
-%              Ex: generation of random numbers, estimation of parameters,
-%              evaluation of pdf and cdf
-% KDETOOLS   - Kernel-density estimation.
-% MISC       - Miscellaneous routines. Ex: numerical integration, smoothing 
-%              spline, binomial coefficient, water density.
-% WDEMOS     - WAFO demos. 
-% DOCS       - Documentation of toolbox, definitions. An overview is given 
-%              in the routine wafomenu. 
-% DATA       - Measurements from marine applications.
-% PAPERS     - Commands that generate figures in selected scientific 
-%              publications.
-% SOURCE     - Fortran and C files. Information on compilation.
-% EXEC       - Executable files (cf. SOURCE), pre-compiled for Solaris, 
-%              Alpha-Dec or Windows. 
+% CYCLES       - Cycle counting, discretization, and crossings. Ex: Rainflow 
+%                cycles and matrix, discretization of loads.
+% DAMAGE       - Calculation of damage. Ex: Damage of a rainflow count or 
+%                matrix, damage matrix, S-N plot.
+% DATA         - Measurements from marine applications.
+% DOCS         - Documentation of toolbox, definitions. An overview is given 
+%                in the routine wafomenu. 
+% EXEC         - Executable files (cf. SOURCE), pre-compiled for Solaris, 
+%                Alpha-Dec or Windows. 
+% GRAPHUTIL    - Utilities for plotting standards and figure manipulation
+% KDETOOLS     - Kernel-density estimation.
+% MARKOV       - Routines for Markov loads, switching Markov loads, and 
+%                their connection to rainflow cycles.
+% MISC         - Miscellaneous routines. Ex: numerical integration, smoothing 
+%                spline, binomial coefficient, water density.
+% MULTIDIM     - Multi-dimensional time series analysis.  (Under construction)
+% NUMDIFFTOOLS - Tools for numerical differantiation
+% ONEDIM       - Data analysis of time series. Example: extraction of 
+%                turning points, estimation of spectrum, covariance function.
+% PAPERS       - Commands that generate figures in selected scientific 
+%                publications.
+% POLYUTIL     - Tools for polynomial manipulations
+% SIMTOOLS     - Simulation of random processes. Ex: spectral simulation, 
+%                simulation of discrete Markov chains, switching Markov
+%                chains, harmonic oscillator
+% SOURCE       - Fortran and C files. Information on compilation.
+% SOURCE64     - Source files and information for 64-bit technology
+% SPEC         - Computation of spectral moments and covariance functions. 
+%                Ex: common spectra implemented, directional spectra, 
+%                bandwidth measures
+% STATISTICS   - Statistical tools and extreme-value distributions.
+%                Ex: generation of random numbers, estimation of parameters,
+%                evaluation of pdf and cdf
+% TRGAUSS      - Modelling with linear, Gaussian waves. Ex: exact 
+%                distributions for wave characteristics, transformed
+%                Gaussian processes
+% WAVEMODELS   - Models for distributions of wave characteristics found in 
+%                the literature. Ex: parametric models for breaking 
+%                limited wave heights.
+% WDEMOS       - WAFO demos. 
+
 % 
 % WAFO homepage: <http://www.maths.lth.se/matstat/wafo/>
 % On the WAFO home page you will find:
@@ -62,6 +67,7 @@
 
 % '$revision$'
 
+% By GL 13 Feb 2011
 % By jr 00.05.26
 % Revised 00.07.10
 % Revised 00.07.13

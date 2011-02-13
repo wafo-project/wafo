572ce52066ca45614dbb774be078f5986d28ff6c
georg.lindgren@gmail.com <georg.lindgren@gmail.com@13e78ada-cb4f-11dd-87b6-9d1a99559f42>
2011-02-13 19:26:11 +0000

    Updated Content to Ver 2.5

--- trunk/wafo25/exec/Contents.m
+++ trunk/wafo25/exec/Contents.m
@@ -1,11 +1,9 @@
 % WAFO  Toolbox /exec   
 % 
 % This directory shall contain a subdirectory with executable files for the
-% computer type at hand (pcwin, sol2, alpha). 
+% computer type at hand (pcwin, pcwin64, sol2, alpha). 
 %
-% When downloading the toolbox from the Internet, this directory is empty. 
-% The executable files shall be placed in this directory; hence, the 
-% structure should look like for example:
-%
-% .../wafo/exec/pcwin/minmax.exe
-
+% When downloading the toolbox from the Internet, this directory  
+% the executable files that have been compiled and tested at the time. 
+% The source files are located in the wafo/source folder and may 
+% be compiled again by the user. 

54657e081375233cc68c3f859f2a18322969a50f
Per.Andreas.Brodtkorb <per.andreas.brodtkorb@gmail.com>
2011-10-05 11:16:26 +0000

    Changed license for some statistics functions based on stixbox to LGPL. (Ref email with Anders Holtsberg dated  24/08/2007)

--- wafo25/statistics/histgrm.m
+++ wafo25/statistics/histgrm.m
@@ -34,6 +34,20 @@ function binwidth = histgrm(x,N,odd,scale,lintype)
 % Revised by es 31.03.2000 Added default in help + lintype  
 % Revised by jr 22.12.1999
 
+%     This program is free software; you can redistribute it and/or modify
+%     it under the terms of the GNU Lesser General Public License as published by
+%     the Free Software Foundation; either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU Lesser General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 if nargin < 2, N = []; end
 if nargin < 3, odd = []; end
 if nargin < 4, scale = []; end
--- wafo25/statistics/invinvnorm.m
+++ wafo25/statistics/invinvnorm.m
@@ -41,6 +41,20 @@ function [x,xlo,xup] = invinvnorm(F,varargin)
 % - added nargchk + comnsize
 % - changed the ad hoc solutions a bit
 
+%     This program is free software; you can redistribute it and/or modify
+%     it under the terms of the GNU Lesser General Public License as published by
+%     the Free Software Foundation; either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU Lesser General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 error(nargchk(3,9,nargin))
 options = struct('covariance',[],'alpha',0.05,...
   'lowertail',true,'logp',false,'abseps',1e-90,'releps',sqrt(eps),'max_iter',500); % default options

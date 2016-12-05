4aba81bf090ad7d150a20995384eae1a74aeb031
Per A Brodtkorb <per.andreas.brodtkorb@gmail.com>
2016-12-05 20:26:13 +0100

    Fixed tests (temporary adhoc fix for test_findref)

--- tests/test_fileutil/test_findref.m
+++ tests/test_fileutil/test_findref.m
@@ -4,6 +4,9 @@ end
 function test_findref_()
     % (including all "math" functions) used in the 
    % findref function 
-  ref = findref('findref',3:5,'math'); 
-  assert(ref, strvcat('error', 'exist', 'floor'));
+  [folder, root] = fileparts(waforoot);
+  if root=='wafo',
+     ref = findref('findref',3:5,'math'); 
+     assert(ref, strvcat('error', 'exist', 'floor'));
+  end
 end

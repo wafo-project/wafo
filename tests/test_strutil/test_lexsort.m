6af5002f53f76c4437d69e1e9424f9c86771d4c5
Per A Brodtkorb <per.andreas.brodtkorb@gmail.com>
2017-05-30 11:39:35 +0200

    Fixed failing test_lexsort.m

--- tests/test_strutil/test_lexsort.m
+++ tests/test_strutil/test_lexsort.m
@@ -3,5 +3,5 @@ function test_suite=test_lexsort()
 end
 function test_lexsort_()
    words = strvcat('octave', 'matlab', 'scilab', 'abc', 'Octave', 'Matlab'); 
-  assert(lexsort(words), strvcat('abc','Matlab','matlab','Octave','octave','scilab')
+  assert(lexsort(words), strvcat('abc','Matlab','matlab','Octave','octave','scilab'))
 end

75b8b4a405c7f702c1833a3da17db3333e11e4dd
Per A Brodtkorb <per.andreas.brodtkorb@gmail.com>
2017-05-30 11:51:49 +0200

    Fixed failing test_parsemfilestr.m

--- tests/test_strutil/test_parsemfilestr.m
+++ tests/test_strutil/test_parsemfilestr.m
@@ -5,6 +5,6 @@ function test_parsemfilestr_()
            % the dewhite function 
  str =  freadtxt('dewhite');  
  [names,synopsis,subroutines,HH,h1] = parsemfilestr(str,'math','trivia') 
- assert(names, strvcat('cell', ¨'error','find','iscell','ischar','isempty',... 
+ assert(names, strvcat('cell', 'error','find','iscell','ischar','isempty',... 
        'isspace','max','nargchk','nargin','numel','size'))
 end
